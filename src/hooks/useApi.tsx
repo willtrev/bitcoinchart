31548c341e999fe0ed86e2f0ac87d3fcd94d9e31
Willian Trevisan <williantrev@gmail.com>
2021-02-07 23:11:29 -0400

    Refatoração do codigo

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -10,34 +10,40 @@ interface ICurrencyFormat {
   rate_float: number;
 }
 
-type Bpi = { [K in currencyTypes]: ICurrencyFormat };
+type Bpi = { [key in currencyTypes]: ICurrencyFormat };
 
 
 export default function useApi() {
-  const [bpi, setBpi] = useState<Bpi[]>([]);
+  const [bpi, setBpi] = useState<Bpi>({} as Bpi);
   const [conversionRate, setConversionRate] = useState({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch(
+        const response = await fetch(
           'https://api.coindesk.com/v1/bpi/currentprice.json'
         );
-        const data = await res.json();
-        setBpi(data.bpi)
-        setConversionRate({
-          eur: (data.bpi['EUR'].rate_float / data.bpi['USD'].rate_float),
-          gbp: (data.bpi['GBP'].rate_float / data.bpi['USD'].rate_float)
-        })
-      } catch (e) {
-        console.error(e)
+
+        const data = await response.json();
+
+        setBpiAndConversionRate(data.bpi);
+
+      } catch (error) {
+        console.error(error)
       }
     }
-    fetchData()
-
+    fetchData();
     setLoading(false);
   }, []);
+
+  const setBpiAndConversionRate = (bpiData: Bpi) => {
+    setBpi(bpiData);
+    setConversionRate({
+      eur: (bpiData.EUR.rate_float / bpiData.USD.rate_float),
+      gbp: (bpiData.GBP.rate_float / bpiData.USD.rate_float)
+    });
+  }
 
   if (!loading) {
     localStorage.setItem('conversionRate', JSON.stringify(conversionRate));
